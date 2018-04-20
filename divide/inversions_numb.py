ebd458cb1cb8bc0a1eb1adbdbc973dd7af89aef4
Pivovarov <danyapivovarov@gmail.com>
2018-04-20 19:19:27 +0300

    attempt to boost algo, in process

--- divide/inversions_numb.py
+++ divide/inversions_numb.py
@@ -1,13 +1,30 @@
 def merge(t1, t2):
+    o1 = o2 = 0
     arr1 = t1[0]
     arr2 = t2[0]
+    arr = []
     counter = 0
-    for i in range(len(arr1)):
-        for j in range(len(arr2)):
-            if arr1[i] > arr2[j]:
-                counter += 1
-    arr1.extend(arr2)
-    return arr1, (counter + t1[1] + t2[1])
+
+    while o1 != len(arr1) and o2 != len(arr2):
+        if arr1[0 + o1] > arr2[0 + o2]:
+            counter += (len(arr2) - o2)
+            arr.append(arr2[0 + o2])
+            o2 += 1
+        else:
+            arr.append(arr1[0 + o1])
+            o1 += 1
+
+    if o1 == len(arr1):
+        while o2 != len(arr2):
+            arr.append(arr2[0 + o2])
+            o2 += 1
+    if o2 == len(arr2):
+        while o1 != len(arr1):
+            arr.append(arr1[0 + o1])
+            counter += len(arr2)
+            o1 += 1
+
+    return arr, (counter + t1[1] + t2[1])
 
 
 def count_numb_of_inversions(arr, l, r):
@@ -19,9 +36,10 @@ def count_numb_of_inversions(arr, l, r):
 
 
 def main():
-    n = int(input())
+    '''n = int(input())
     init_arr = [int(x) for x in input().split(' ')]
-    assert n == len(init_arr)
+    assert n == len(init_arr)'''
+    init_arr = [3, 5, 1, 4, 6, 2, 7, 8]
     print(count_numb_of_inversions(init_arr, 0, len(init_arr)-1)[1])
 
 
