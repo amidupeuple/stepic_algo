b22dfbc7489861381e01693de7a7af56f713203a
Danya <danyapivovarov@gmail.com>
2018-04-21 22:30:42 +0300

    merge sort + inversion counter

--- divide/inversions_numb.py
+++ divide/inversions_numb.py
@@ -6,22 +6,21 @@ def merge(t1, t2):
     counter = 0
 
     while o1 != len(arr1) and o2 != len(arr2):
-        if arr1[0 + o1] > arr2[0 + o2]:
-            counter += (len(arr2) - o2)
-            arr.append(arr2[0 + o2])
+        if arr1[o1] > arr2[o2]:
+            counter += len(arr1) - o1
+            arr.append(arr2[o2])
             o2 += 1
         else:
-            arr.append(arr1[0 + o1])
+            arr.append(arr1[o1])
             o1 += 1
 
     if o1 == len(arr1):
         while o2 != len(arr2):
-            arr.append(arr2[0 + o2])
+            arr.append(arr2[o2])
             o2 += 1
-    if o2 == len(arr2):
+    elif o2 == len(arr2):
         while o1 != len(arr1):
-            arr.append(arr1[0 + o1])
-            counter += len(arr2)
+            arr.append(arr1[o1])
             o1 += 1
 
     return arr, (counter + t1[1] + t2[1])
@@ -36,10 +35,9 @@ def count_numb_of_inversions(arr, l, r):
 
 
 def main():
-    '''n = int(input())
+    n = int(input())
     init_arr = [int(x) for x in input().split(' ')]
-    assert n == len(init_arr)'''
-    init_arr = [3, 5, 1, 4, 6, 2, 7, 8]
+    assert n == len(init_arr)
     print(count_numb_of_inversions(init_arr, 0, len(init_arr)-1)[1])
 
 
