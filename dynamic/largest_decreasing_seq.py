cb71b0e42d56f4b55bf7aee65cc3679d30e4c481
dpivovar <danyapivovarov@gmail.com>
2018-05-18 16:43:48 +0300

    lds, try to achieve O(nlogn)

--- dynamic/largest_decreasing_seq.py
+++ dynamic/largest_decreasing_seq.py
@@ -1,48 +1,114 @@
-def lds(arr):
-    D = [1 for i in range(len(arr))]
-    for i in range(1, len(arr)):
-        for j in range(i):
-            if arr[i] <= arr[j] and (D[j] + 1) > D[i]:
-                D[i] = D[j] + 1
+from bisect import bisect_left
 
-    m = 0
-    for i in range(len(D)):
-        if D[i] > m:
-            m = D[i]
-
-    s = m
-    res = []
-    if m == 1:
-        m = 0
-    prev = arr[m]
-    while len(res) != s:
-        if arr[m] >= prev:
-            prev = arr[m]
-            res.append(m + 1)
-            if m != 0:
-                m = D[D[m]] - 1
-        else:
-            m -= 1
-    res.reverse()
-    return s, res
-
-
-def test():
+def test_lds():
     n, a = lds([1])
-    assert n == 1 and len(a) == 1 and a[0] == 1
+    assert n == 1
     n, a = lds([5, 3, 4, 4, 2])
-    assert n == 4 and len(a) == 4
+    assert n == 4
+    n, a = lds([5, 3, 4, 4, 2, 4, 4, 4, 4, 4])
+    assert n == 8
+
+
+def test_find_pos():
+    n = find_pos([5], 0, 0, 3)
+    assert n == 1
+    n = find_pos([5, 4, 4, 2], 0, 3, 3)
+    assert n == 3
+    n = find_pos([], 0, 0, 1)
+    assert n == 0
+    n = find_pos([5, 4, 4, 3, 3, 3, 3, 3, 3, 2], 0, 9, 3)
+    assert n == 9
+
+
+def lis(a):
+    seqs = []
+    seqs.append(a[0])
+    l = 1
+    indxs = []
+    indxs.append([0])
+
+    for i in range(1, len(a)):
+        j = bisect_left(seqs, a[i])
+        if j == 0:
+            seqs[0] = a[i]
+            indxs[0] = [i]
+        elif j == len(seqs):
+            seqs.append(a[i])
+            l += 1
+            tmp = indxs[len(indxs) - 1][:]
+            tmp.append(i)
+            indxs.append(tmp)
+        else:
+            if j + 1 == len(seqs):
+                seqs.append(a[i])
+                l += 1
+                tmp = indxs[len(indxs) - 1][:]
+                tmp.append(i)
+                indxs.append(tmp)
+            else:
+                seqs[j] = a[i]
+                indxs[j][len(indxs[j]) - 1] = i
+
+    print('E N D')
+
+
+def find_pos(a, l, r, e):
+    while l <= r and a:
+        m = (l + r) // 2
+        if a[m] < e:
+            return find_pos(a, l, m - 1, e)
+        elif a[m] > e:
+            return find_pos(a, m + 1, r, e)
+        else:
+            while a[m] == e:
+                m += 1
+                if m == len(a):
+                    break
+            return m
+    return l
+
+
+def lds(a):
+    seqs = []
+    seqs.append(a[0])
+    l = 1
+    indxs = []
+    indxs.append([0])
+
+    for i in range(1, len(a)):
+        j = find_pos(seqs, 0, len(seqs) - 1, a[i])
+        if j == len(seqs):
+            seqs.append(a[i])
+            l += 1
+            tmp = indxs[len(indxs) - 1][:]
+            tmp.append(i)
+            indxs.append(tmp)
+        elif j == 0:
+            seqs[0] = a[i]
+            indxs[0] = [i]
+        else:
+            if j == len(seqs):
+                seqs.append(a[i])
+                l += 1
+                tmp = indxs[len(indxs) - 1][:]
+                tmp.append(i)
+                indxs.append(tmp)
+            else:
+                seqs[j] = a[i]
+                indxs[j][len(indxs[j]) - 1] = i
+    return l, indxs[len(indxs) - 1]
 
 
 def main():
-    # n = int(input())
-    # arr = [int(x) for x in input().split(' ')]
-    # assert n == len(arr)
-    arr = [5, 3, 4, 4, 2]
+    n = int(input())
+    arr = [int(x) for x in input().split(' ')]
+    assert n == len(arr)
     n, arr_ind = lds(arr)
     print(str(n))
-    print(' '.join([str(x) for x in arr_ind]))
+    print(' '.join([str(x + 1) for x in arr_ind]))
 
 
 if __name__ == "__main__":
-    test()
+    # test_lds()
+    # test_find_pos()
+    main()
