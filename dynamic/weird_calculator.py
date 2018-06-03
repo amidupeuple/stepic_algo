c3aa09b5f7d2b775d2c4f8a708d6273e1d24c0c1
Danya <danyapivovarov@gmail.com>
2018-06-03 12:55:04 +0300

    weird calc: completed (weird)

--- dynamic/weird_calculator.py
+++ dynamic/weird_calculator.py
@@ -1,21 +1,43 @@
 def find_min_number_of_operations(n):
     d = [float("inf") for i in range(n + 1)]
-    arr = [0 for i in range(n)]
-    arr[0] = 1
+    arr = ["" for i in range(n)]
+    arr[0] = "1"
     d[0] = 0
     d[1] = 0
     for i in range(2, n + 1):
         for j in range(1, 4):
             if j == 1:
                 d[i] = min(d[i], d[i-1] + 1)
-                arr[d[i-1] + 1] = i
+                arr[d[i-1] + 1] += str(i)
             elif j == 2 and (i % 2) == 0:
                 d[i] = min(d[i], d[int(i / 2)] + 1)
-                arr[d[int(i / 2)] + 1] = i
+                arr[d[int(i / 2)] + 1] += str(i)
             elif j == 3 and (i % 3) == 0:
                 d[i] = min(d[i], d[int(i / 3)] + 1)
-                arr[d[int(i / 3)] + 1] = i
-    return d[n], arr[0:(d[n] + 1)]
+                arr[d[int(i / 3)] + 1] += str(i)
+    arr = [float("-inf") for i in range(n + 1)]
+    arr[-1] = d[n]
+    return d[n], find_sequence(arr)
+
+
+def find_sequence(arr):
+    for i in range(len(arr) - 1, -1, -1):
+        for j in range(1, 4):
+            if j == 1 and arr[i] > 0:
+                arr[i-1] = max(arr[i-1], arr[i] - 1)
+            elif j == 2 and (i % 2 == 0) and arr[i] > 0:
+                arr[int(i / 2)] = max(arr[int(i / 2)], arr[i] - 1)
+            elif j == 3 and (i % 3 == 0) and arr[i] > 0:
+                arr[int(i / 3)] = max(arr[int(i / 3)], arr[i] - 1)
+    c = 1
+    res = [1]
+    prev = 1
+    for i in range(len(arr)):
+        if arr[i] == c and (i == prev + 1 or i == prev * 2 or i == prev * 3):
+            prev = i
+            c += 1
+            res.append(i)
+    return res
 
 
 def test():
@@ -41,5 +63,5 @@ def main():
 
 
 if __name__ == "__main__":
-    # main()
-    test()
+    main()
+    # test()b
